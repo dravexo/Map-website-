3a84291d8fb4bfe0ea790c49e7d89be40e67db5b
dravexo <nishantkumar9375@gmail.com>
2026-03-04 17:26:02 +0530

    Refactor map initialization and marker creation

--- Script.js
+++ Script.js
@@ -1,19 +1,17 @@
-// Map initialize
-var map = L.map('map').setView([20, 0], 2);  // World center, zoom 2
+// World map initialize
+var map = L.map('map').setView([20, 0], 2);  // Center on world, zoom level 2
 
-// Free OpenStreetMap tiles
+// Free map tiles from OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 18,
 }).addTo(map);
 
-// Agra/Taj Mahal marker (Nishant special!)
-var markerIndia = L.marker([27.1751, 78.0421]).addTo(map);
-markerIndia.bindPopup("<b>Agra, Uttar Pradesh</b><br>Taj Mahal yahan hai!<br>Nishant ka area ❤️").openPopup();
+// Agra marker (Taj Mahal)
+L.marker([27.1751, 78.0421]).addTo(map)
+  .bindPopup("<b>Agra, Uttar Pradesh</b><br>Taj Mahal yahan hai!<br>Nishant ka ghar ❤️")
+  .openPopup();
 
-// Ek aur example (New York)
+// New York example
 L.marker([40.7128, -74.0060]).addTo(map)
-  .bindPopup("<b>New York, USA</b><br>Big Apple!");
-
-// Optional: Console log for testing
-console.log("Map loaded! Visitors counter bhi check karo.");
+  .bindPopup("<b>New York, USA</b><br>Statue of Liberty");
